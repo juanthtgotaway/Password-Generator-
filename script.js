42de4e0375e3a04b9a2bdb45136ba1bd653af1a5
Juan Acevedo <jmamartinez18@gmail.com>
2023-11-04 10:42:18 -0600

    popup window for amount of numbers

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var passwordLength = prompt("How long would you like your password (must be between 8-128 characters)?");
+
+  passwordLength = Number(passwordLength);
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert ("Password must be from 8-128.")
+  }
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
